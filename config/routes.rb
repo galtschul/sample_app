5e762ff52ee32155585070ce982e6d7e4369a731
Gregg Altschul <galtschul@gmail.com>
2013-05-20 20:39:51 -0400

    user auth

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
+#  match '/edit_user', to: 'users#edit', via: :edit
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,11 @@
 include ApplicationHelper
 
-def valid_signin(user)
+def sign_in(user)
+	visit signin_path
   fill_in "Email",    with: user.email.upcase
   fill_in "Password", with: user.password
   click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
 end
 
 def fill_form
